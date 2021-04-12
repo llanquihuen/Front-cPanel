2911a836bf1755300b2c05696009b4a5076162a0
Seth <llanquihuen@gmail.com>
2021-04-11 22:34:21 -0400

    RegionesyComunas

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -3,7 +3,7 @@ import React,{useState, useRef, useEffect} from 'react'
 import {useSelector} from 'react-redux'
 import { makeStyles } from '@material-ui/core/styles';
 import {pink} from '@material-ui/core/colors'
-import {Paper,TextField, Button, Grid, MenuItem} from '@material-ui/core'
+import {Paper,TextField, Button, Grid, MenuItem, NativeSelect, InputLabel} from '@material-ui/core'
 import { useMediaQuery } from 'react-responsive';
 
 import HeroSwiper from '../HeroSwiper';
@@ -68,8 +68,27 @@ root2: {
     },
     '& .MuiInputLabel-outlined.MuiInputLabel-shrink': {
         transform:'translate(7px,-15px) scale(.85)'
+    },
+    '& .MuiInputBase-root': {
+        fontSize:'1.7rem',
+        color: pink[900],
+    },
+    '& .MuiInput-underline:before':{
+        borderRadius:5,
+        border:'1px solid blue',
+        paddingBottom:30,
+        borderColor: pink[200],
+        
+    },
+    '& .MuiInput-underline:after':{
+        borderColor: pink[300],
+    },
+    '& .MuiInput-underline:hover:not(.Mui-disabled):before': {
+        borderColor: pink[100],
+      },
+    '& .MuiNativeSelect-select:focus':{
+        backgroundColor: 'transparent'
     }
-    
 },
 nonono:{
     '& .MuiOutlinedInput-root': {
@@ -126,6 +145,7 @@ const Users = (props) => {
     const inputRut = useRef(null);
     const classes = useStyles();
     const isMobile = useMediaQuery({ query: '(max-width: 360px)' })
+    const isSmall = useMediaQuery({query:'(max-width:567px)'})
     const isMediumLong = useMediaQuery({ query: '(max-width: 1300px)' })
 
 
@@ -134,7 +154,10 @@ const Users = (props) => {
         email:"",
         rut:"",
         telefono:"",
+        region:"",
+        comuna:"",
         direccion:"",
+
     })
     
 
@@ -253,7 +276,7 @@ const Users = (props) => {
         },
             {
                 "NombreRegion": "Región del Maule",
-                "comunas": ["Talca", "ConsVtución", "Curepto", "Empedrado", "Maule", "Pelarco", "Pencahue", "Río Claro", "San Clemente", "San Rafael", "Cauquenes", "Chanco", "Pelluhue", "Curicó", "Hualañé", "Licantén", "Molina", "Rauco", "Romeral", "Sagrada Familia", "Teno", "Vichuquén", "Linares", "Colbún", "Longaví", "Parral", "ReVro", "San Javier", "Villa Alegre", "Yerbas Buenas"]
+                "comunas": ["Talca", "Consitución", "Curepto", "Empedrado", "Maule", "Pelarco", "Pencahue", "Río Claro", "San Clemente", "San Rafael", "Cauquenes", "Chanco", "Pelluhue", "Curicó", "Hualañé", "Licantén", "Molina", "Rauco", "Romeral", "Sagrada Familia", "Teno", "Vichuquén", "Linares", "Colbún", "Longaví", "Parral", "Retiro", "San Javier", "Villa Alegre", "Yerbas Buenas"]
         },
             {
                 "NombreRegion": "Región del Biobío",
@@ -269,7 +292,7 @@ const Users = (props) => {
         },
             {
                 "NombreRegion": "Región de Los Lagos",
-                "comunas": ["Puerto Montt", "Calbuco", "Cochamó", "Fresia", "FruVllar", "Los Muermos", "Llanquihue", "Maullín", "Puerto Varas", "Castro", "Ancud", "Chonchi", "Curaco de Vélez", "Dalcahue", "Puqueldón", "Queilén", "Quellón", "Quemchi", "Quinchao", "Osorno", "Puerto Octay", "Purranque", "Puyehue", "Río Negro", "San Juan de la Costa", "San Pablo", "Chaitén", "Futaleufú", "Hualaihué", "Palena"]
+                "comunas": ["Puerto Montt", "Calbuco", "Cochamó", "Fresia", "Frutillar", "Los Muermos", "Llanquihue", "Maullín", "Puerto Varas", "Castro", "Ancud", "Chonchi", "Curaco de Vélez", "Dalcahue", "Puqueldón", "Queilén", "Quellón", "Quemchi", "Quinchao", "Osorno", "Puerto Octay", "Purranque", "Puyehue", "Río Negro", "San Juan de la Costa", "San Pablo", "Chaitén", "Futaleufú", "Hualaihué", "Palena"]
         },
             {
                 "NombreRegion": "Región Aisén del Gral. Carlos Ibáñez del Campo",
@@ -281,71 +304,94 @@ const Users = (props) => {
         },
             {
                 "NombreRegion": "Región Metropolitana de Santiago",
-                "comunas": ["Cerrillos", "Cerro Navia", "Conchalí", "El Bosque", "Estación Central", "Huechuraba", "Independencia", "La Cisterna", "La Florida", "La Granja", "La Pintana", "La Reina", "Las Condes", "Lo Barnechea", "Lo Espejo", "Lo Prado", "Macul", "Maipú", "Ñuñoa", "Pedro Aguirre Cerda", "Peñalolén", "Providencia", "Pudahuel", "Quilicura", "Quinta Normal", "Recoleta", "Renca", "San Joaquín", "San Miguel", "San Ramón", "Vitacura", "Puente Alto", "Pirque", "San José de Maipo", "Colina", "Lampa", "TilVl", "San Bernardo", "Buin", "Calera de Tango", "Paine", "Melipilla", "Alhué", "Curacaví", "María Pinto", "San Pedro", "Talagante", "El Monte", "Isla de Maipo", "Padre Hurtado", "Peñaflor"]
+                "comunas": ["Cerrillos", "Cerro Navia", "Conchalí", "El Bosque", "Estación Central", "Huechuraba", "Independencia", "La Cisterna", "La Florida", "La Granja", "La Pintana", "La Reina", "Las Condes", "Lo Barnechea", "Lo Espejo", "Lo Prado", "Macul", "Maipú", "Ñuñoa", "Pedro Aguirre Cerda", "Peñalolén", "Providencia", "Pudahuel", "Quilicura", "Quinta Normal", "Recoleta", "Renca", "San Joaquín", "San Miguel", "San Ramón", "Vitacura", "Puente Alto", "Pirque", "San José de Maipo", "Colina", "Lampa", "Tiltíl", "San Bernardo", "Buin", "Calera de Tango", "Paine", "Melipilla", "Alhué", "Curacaví", "María Pinto", "San Pedro", "Talagante", "El Monte", "Isla de Maipo", "Padre Hurtado", "Peñaflor"]
         }]
     }
-    
-    const names = [
-        'Oliver Hansen',
-        'Van Henry',
-        'April Tucker',
-        'Ralph Hubbard',
-        'Omar Alexander',
-        'Carlos Abbott',
-        'Miriam Wagner',
-        'Bradley Wilkerson',
-        'Virginia Andrews',
-        'Kelly Snyder',
-      ];
+    const lookUp =(name)=>{
+        for(let i=0;i<RegionesYcomunas.regiones.length;i++){
+            if (RegionesYcomunas.regiones[i].NombreRegion===name){
+                return RegionesYcomunas.regiones[i].comunas.sort(Intl.Collator().compare)
+            }
+        }
+    }
 
-      console.log(names)
-    //   for(i=1;i<RegionesYcomunas.regiones)
-      console.log(RegionesYcomunas.regiones[0].NombreRegion)
+    let cm=(lookUp(userData.region))
+    let envio = 0 
+
+      if (userData.region==='Región Metropolitana de Santiago'){
+          if (userData.comuna=== ""||userData.comuna === cm[0]||userData.comuna === cm[1]||userData.comuna === cm[2]||userData.comuna === cm[5]||userData.comuna === cm[7]||userData.comuna ===cm[9]||userData.comuna ===cm[13]||userData.comuna === cm[19]||userData.comuna === cm[26]||userData.comuna === cm[27]||userData.comuna === cm[29]||userData.comuna === cm[30]||userData.comuna === cm[32]||userData.comuna === cm[34]||userData.comuna === cm[42]||userData.comuna === cm[44]||userData.comuna === cm[46]||userData.comuna === cm[48]||userData.comuna === cm[49]){
+              console.log('demasiado lejos')
+          }else{
+              envio=3000
+          }
+      }
+    //   console.log(RegionesYcomunas.regiones[2].NombreRegion)
+
         return (
         <div style={{display:'flex', flexDirection:'column'}}>
             {/* <HeroSwiper updateLista={updateLista}/> */}
             <div style={{height:'100%',paddingBottom:'6rem',minHeight:'800px', background:'linear-gradient(125deg, rgba(251,168,179,.6) 0%, rgba(254,228,232,.6) 100%)'}}>
                 <h1 style={{textAlign:'center', margin:0, padding:'5rem'}}>DATOS PARA LA ENTREGA</h1>
                 <div    className={isMediumLong?classes.rootMobile:classes.root}>
-                    <Paper style={{ display:'flex', flexDirection:isMediumLong?'column':'row',justifyContent:'space-around',background:'linear-gradient(180deg, rgba(255,247,250,1) 0%, rgba(255,235,241,1) 100%)', borderRadius:'19px', }}  elevation={9} >
-                    <div style={{width:isMediumLong?'95%':'50%',margin:'auto', padding:isMobile?'0 0':'0 3rem'}}>
-                        <h2 style={{textAlign:'center', paddingTop:'3rem'}}>Completa el formulario para el envío</h2>
-                    <form className={classes.root2} noValidate autoComplete="off">
+                    <Paper style={{ display:'flex', flexDirection:isMediumLong?'column':'row',minHeight:750,justifyContent:'space-between',background:'linear-gradient(180deg, rgba(255,247,250,1) 0%, rgba(255,235,241,1) 100%)', borderRadius:'19px', }}  elevation={9} >
+                    <div style={{width:isMediumLong?'95%':'50%',margin:'27px auto', padding:isSmall?'0 0':'0 3rem'}}>
+                        <h2 style={{textAlign:'center'}}>Completa el formulario para el envío</h2>
+                    <form style={{display:'flex',flexDirection:'column',gap:17,height:'100%'}}className={classes.root2} noValidate autoComplete="off">
                         
                         <TextField inputProps={{style: {fontSize: 16}}}  InputLabelProps={{style: {fontSize: 15}}}  required={true} label="Nombre" variant="outlined" value={userData.name} onChange={(e)=> setUserData({...userData, nombre:e.target.value})}/>
                         <div>
-                        <TextField inputProps={{style: {fontSize: 16}}}  InputLabelProps={{style: {fontSize: 15}}} className={testMail? classes.sisisi:classes.nonono} required={true} label="Email" variant="outlined" value={userData.email} onChange={(e)=>validateEmail(e.target.value)} />
-                        {!testMail?<p style={{height:0,padding:'0 5', color:'crimson'}}>Escriba un mail válido</p>:<></>}
+                        <TextField inputProps={{style: {fontSize: 16}}}  style={{width:isSmall?'100%':'35ch'}} InputLabelProps={{style: {fontSize: 15}}} className={testMail? classes.sisisi:classes.nonono} required={true} label="Email" variant="outlined" value={userData.email} onChange={(e)=>validateEmail(e.target.value)} />
+                        {!testMail?<p style={{height:0,padding:'0 5', marginBottom:17, color:'crimson'}}>Escriba un mail válido</p>:<></>}
                         </div>
-                        <div style={{display:'flex',justifyContent:'space-between', gap:isMobile?5:10}}>
-                            
+                        <div style={{display:'flex',flexDirection:isSmall?'column':'row',justifyContent:'space-between', gap:isSmall?20:10, marginBottom:25}}>
+
                             <div>
-                            <TextField inputProps={{maxLength: 12, style: {fontSize:isMobile?15:16, paddingLeft:isMobile?8:'auto', paddingRight:8,}}}  className={testRut? classes.sisisi:classes.nonono} InputLabelProps={{style: {fontSize: 15}}} style={{width:isMobile?'60%':'25ch'}} ref={inputRut}  onBlur={(e)=>puntosYGuion(e.target.value)} required={true} label="RUT" variant="outlined" value={userData.rut} onChange={(e)=>validateRut(e.target.value)}  />
-                            {!testRut?<p style={{height:0,padding:'0 5', color:'crimson'}}>Escriba un RUT válido</p>:<></>}
+                            <TextField inputProps={{maxLength: 12, style: {fontSize:isMobile?15:16, paddingLeft:isMobile?8:'auto', paddingRight:8,}}}  className={testRut? classes.sisisi:classes.nonono} InputLabelProps={{style: {fontSize: 15}}} style={{width:isSmall?'100%':'25ch'}} ref={inputRut}  onBlur={(e)=>puntosYGuion(e.target.value)} required={true} label="RUT" variant="outlined" value={userData.rut} onChange={(e)=>validateRut(e.target.value)}  />
+                            {!testRut?<p style={{height:0,padding:'0 5', marginBottom:17,color:'crimson'}}>Escriba un RUT válido</p>:<></>}
                             </div>
-                            <TextField inputProps={{style: {fontSize:isMobile?15:16}}}  InputLabelProps={{style: {fontSize: 15}}} style={{width:'25ch'}} required={true} label="Teléfono" variant="outlined" />
+                            <TextField inputProps={{style: {fontSize:isMobile?15:16}}}  InputLabelProps={{style: {fontSize: 15}}} style={{width:isSmall?'100%':'25ch'}} required={true} label="Teléfono" variant="outlined" onChange={(e)=> setUserData({...userData, telefono:e.target.value})} />
                         </div>
-                        <TextField inputProps={{style:{fontSize:16}, MenuProps: {disableScrollLock: true}}} InputLabelProps={{style: {fontSize: 15}}} id="select" label="Región" value="20" select>
+                        
+                        <NativeSelect required={true} style={{marginBottom:15}}inputProps={{style:{fontSize:15, paddingBottom:18, paddingLeft:16}, /*MenuProps: {disableScrollLock: true}*/}} id="select" label="Región" value={userData.region} onChange={(e)=> setUserData({...userData, region:e.target.value})}>
+                        <option disabled value="">Región</option>
                         {RegionesYcomunas.regiones.map((name) => (
-                        <MenuItem key={name.NombreRegion} value={name}>
+                        <option key={name.NombreRegion} value={name.NombreRegion}>
                         {name.NombreRegion}
-                        </MenuItem>
+                        </option>
                         ))}
-                        </TextField>
-                        <TextField inputProps={{style: {fontSize: 16}}}  InputLabelProps={{style: {fontSize: 15}}} required={true} label="Dirección para la entrega" variant="outlined" />
-                        <Button style={{padding:'1em', background:'pink',fontSize:'1.3em',margin:'auto', textAlign:'center'}}>Pagar</Button>
+                        </NativeSelect>
+                        {userData.region? 
+                        <NativeSelect inputProps={{style:{fontSize:15, paddingBottom:18, paddingLeft:16}, /*MenuProps: {disableScrollLock: true}*/}} id="select" label="Región" value={userData.comuna} onChange={(e)=> setUserData({...userData, comuna:e.target.value})}>
+                        <option disabled value="">Comuna</option>
+                        {lookUp(userData.region).map((name) => (
+                        <option key={name} value={name}>
+                        {name}
+                        </option>
+                        ))}
+                        </NativeSelect>:<></>}
+            
+                        <TextField inputProps={{style: {fontSize: 16}}}  InputLabelProps={{style: {fontSize: 15, paddingBottom:10}}} required={true} label="Dirección" variant="outlined" onChange={(e)=> setUserData({...userData, direccion:e.target.value})} />
+                        {envio!==0?<p></p>: userData.region==='Región Metropolitana de Santiago'?<p style={{ width:'80%', margin:'1rem auto', fontSize:'1em'}}>Envíos fuera de santiago urbano por pagar en Starken</p>:<p style={{ width:'80%', margin:'1rem auto', fontSize:'1em'}}>Envíos fuera de santiago por pagar en Starken</p>}
+
+                        <Button style={{padding:'1em', background:'pink',fontSize:'1.3em',margin:'auto', textAlign:'center'}}>Pagar ${numberWithDots(sumaPrecios+envio)}</Button>
+                        <div style={{height:'50px'}}></div>
+
                     </form>
                     </div>
                     {isMediumLong?  <hr style={{background:'white', width:'90%', borderColor:'pink'}}></hr>:<hr style={{background:pink[50],height:'80%', width:'4px', borderRadius:'5px',borderColor:pink[50], margin:'auto'}}></hr>}
                   
                     <div className={classes.scrollbar} style={{background:'transparent',height:'100%',overflowY:'scroll', width:isMediumLong?'80%':'50%', margin:'auto'}}>
-                        <h2 style={{textAlign:'center'}}>Detalle Compra</h2>
+                        <h2 style={{textAlign:'center',paddingTop:27}}>Detalle Compra</h2>
                 {intersection.map((post)=>(
-                    <Grid style={{ width:'80%', margin:'4rem auto'}}  key={post._id} item >
+                    <Grid style={{ width:'80%', margin:'2rem auto'}}  key={post._id} item >
                     <DetalleCompra post={post} storage={listaPedido}/>
                     </Grid>
                 ))}  
-                <p style={{ width:'80%', margin:'4rem auto', fontSize:'2em'}} >Total: {numberWithDots(sumaPrecios)}</p>    
+                <p style={{ width:'80%', margin:'1rem auto', fontSize:envio!==0?'1.5em':'2em'}} >Total: {numberWithDots(sumaPrecios)}</p>    
+                {envio!==0?<p style={{ width:'80%', margin:'1rem auto', fontSize:'1.5em'}} >Envío: {numberWithDots(envio)}</p>: userData.region==='Región Metropolitana de Santiago'?<p style={{ width:'80%', margin:'1rem auto', fontSize:'1em'}}>Envíos fuera de santiago urbano por pagar en Starken</p>:<p style={{ width:'80%', margin:'1rem auto', fontSize:'1em'}}>Envíos fuera de santiago por pagar en Starken</p>}
+                {envio!==0?<p style={{ width:'80%', margin:'2rem auto', fontSize:'2em'}} >Total: {numberWithDots(sumaPrecios+envio)}</p>:<></>}
+                <div style={{height:'50px'}}></div>
+
                 </div> 
                     </Paper>
                 </div>
